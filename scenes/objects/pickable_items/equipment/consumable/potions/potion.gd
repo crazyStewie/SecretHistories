5a3efe3ad5adaaec39501d8799b270f86278f34d
Chris <Moses2k@gmail.com>
2024-06-04 23:22:05 -0700

    2 basic test potions for testing stack system

---
+++ scenes/objects/pickable_items/equipment/consumable/potions/potion.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+class_name PotionItem
+
+# Should eventually choose a psuedorandom potion bottle, color, effect - and always choose the same for future potions of this type (roguelike ID game)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
